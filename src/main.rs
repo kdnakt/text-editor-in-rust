e078d6c83fc7b9d9c3c41e722d3c45e98de15bcd
kdnakt <a.kid.1985@gmail.com>
2025-03-08 08:44:51 +0900

    entering raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::Read;
 
 fn main() {
+    enable_raw_mode().unwrap();
     for b in std::io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{}", c);
         if c == 'q' {
+            disable_raw_mode().unwrap();
             break;
         }
     }
