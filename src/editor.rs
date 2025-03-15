4165a5138a28791349ecd3ee3cc8c2f0f1525867
kdnakt <a.kid.1985@gmail.com>
2025-03-15 15:00:34 +0900

    break with should_quit

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,5 @@
 use crossterm::event::KeyCode::Char;
+use crossterm::event::{KeyEvent, KeyModifiers};
 use crossterm::{
     event::{read, Event::Key},
     terminal::{disable_raw_mode, enable_raw_mode},
@@ -12,22 +13,34 @@ impl Editor {
         Self { should_quit: false }
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         if let Err(e) = self.repl() {
             panic!("{e:#?}");
         }
         print!("Goodbye.\r\n");
     }
 
-    fn repl(&self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode()?;
         loop {
-            if let Key(event) = read()? {
-                println!("{event:?}\r");
-                if let Char(c) = event.code {
-                    if c == 'q' {
-                        break;
+            if let Key(KeyEvent {
+                code,
+                modifiers,
+                kind,
+                state,
+            }) = read()?
+            {
+                println!(
+                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r"
+                );
+                match code {
+                    Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
                     }
+                    _ => (),
+                }
+                if self.should_quit {
+                    break;
                 }
             }
         }
