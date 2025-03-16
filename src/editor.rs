1f170154ddb7a873c2c5e2a9580feab68fd0aa48
kdnakt <a.kid.1985@gmail.com>
2025-03-16 22:11:42 +0900

    clear screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,7 @@
 use crossterm::event::KeyCode::Char;
 use crossterm::event::{KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType};
 use crossterm::{
     event::{read, Event::Key},
     terminal::{disable_raw_mode, enable_raw_mode},
@@ -14,14 +16,24 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
+        Self::initialize().unwrap();
         if let Err(e) = self.repl() {
             panic!("{e:#?}");
         }
         print!("Goodbye.\r\n");
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn initialize() -> Result<(), std::io::Error> {
         enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = std::io::stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
             if let Key(KeyEvent {
                 code,
