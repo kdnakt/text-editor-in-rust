aa2c2a69d651ba5bcc1c7455927716c01474393b
kdnakt <a.kid.1985@gmail.com>
2025-03-21 00:13:51 +0900

    refactor

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,3 +1,5 @@
+use std::io::Error;
+
 use crossterm::{
     execute, queue,
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
@@ -6,7 +8,7 @@ use crossterm::{
 pub struct Terminal {}
 
 impl Terminal {
-    pub fn initialize() -> Result<(), std::io::Error> {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
         Self::draw_rows()?;
@@ -14,29 +16,29 @@ impl Terminal {
         Ok(())
     }
 
-    pub fn show_cursor() -> Result<(), std::io::Error> {
+    pub fn show_cursor() -> Result<(), Error> {
         queue!(std::io::stdout(), crossterm::cursor::Show)?;
         Ok(())
     }
 
-    pub fn hide_cursor() -> Result<(), std::io::Error> {
+    pub fn hide_cursor() -> Result<(), Error> {
         queue!(std::io::stdout(), crossterm::cursor::Hide)?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
+    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), Error> {
         let command = crossterm::cursor::MoveTo(x, y);
         execute!(std::io::stdout(), command)?;
         Ok(())
     }
 
-    pub fn clear_screen() -> Result<(), std::io::Error> {
+    pub fn clear_screen() -> Result<(), Error> {
         let mut stdout = std::io::stdout();
         execute!(stdout, Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn draw_rows() -> Result<(), std::io::Error> {
+    pub fn draw_rows() -> Result<(), Error> {
         for _ in 0..crossterm::terminal::size().unwrap().1 {
             print!("~\r\n");
         }
@@ -45,7 +47,7 @@ impl Terminal {
         Ok(())
     }
 
-    pub fn terminate() -> Result<(), std::io::Error> {
+    pub fn terminate() -> Result<(), Error> {
         disable_raw_mode()?;
         Ok(())
     }
