eace76cabff297ad018d5ef95cd5a137abe4e38c
kdnakt <a.kid.1985@gmail.com>
2025-03-21 00:29:46 +0900

    refactor

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,4 +1,4 @@
-use std::io::Error;
+use std::io::{stdout, Error};
 
 use crossterm::{
     execute, queue,
@@ -17,28 +17,28 @@ impl Terminal {
     }
 
     pub fn show_cursor() -> Result<(), Error> {
-        queue!(std::io::stdout(), crossterm::cursor::Show)?;
+        queue!(stdout(), crossterm::cursor::Show)?;
         Ok(())
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
-        queue!(std::io::stdout(), crossterm::cursor::Hide)?;
+        queue!(stdout(), crossterm::cursor::Hide)?;
         Ok(())
     }
 
     pub fn print(string: &str) -> Result<(), Error> {
-        queue!(std::io::stdout(), crossterm::style::Print(string))?;
+        queue!(stdout(), crossterm::style::Print(string))?;
         Ok(())
     }
 
     pub fn move_cursor_to(x: u16, y: u16) -> Result<(), Error> {
         let command = crossterm::cursor::MoveTo(x, y);
-        execute!(std::io::stdout(), command)?;
+        execute!(stdout(), command)?;
         Ok(())
     }
 
     pub fn clear_screen() -> Result<(), Error> {
-        let mut stdout = std::io::stdout();
+        let mut stdout = stdout();
         execute!(stdout, Clear(ClearType::All))?;
         Ok(())
     }
@@ -48,7 +48,7 @@ impl Terminal {
             print!("~\r\n");
         }
         let command = crossterm::cursor::MoveTo(0, 0);
-        crossterm::execute!(std::io::stdout(), command)?;
+        crossterm::execute!(stdout(), command)?;
         Ok(())
     }
 
