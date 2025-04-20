8a965bb9235313ffe40ed504d4b063dc207684f4
kdnakt <a.kid.1985@gmail.com>
2025-04-20 10:41:36 +0900

    convert event to editor command

--- src/editor/editorcommand.rs
+++ src/editor/editorcommand.rs
@@ -1,4 +1,4 @@
-use crossterm::event::Event;
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 
 use super::terminal::Size;
 
@@ -23,6 +23,29 @@ impl TryFrom<Event> for EditorCommand {
     type Error = String;
 
     fn try_from(event: Event) -> Result<Self, Self::Error> {
-        todo!()
+        match event {
+            Event::Key(KeyEvent {
+                code, modifiers, ..
+            }) => match (code, modifiers) {
+                (KeyCode::Char('q'), KeyModifiers::CONTROL) => Ok(Self::Quit),
+                (KeyCode::Up, _) => Ok(Self::Move(Direction::Up)),
+                (KeyCode::Down, _) => Ok(Self::Move(Direction::Down)),
+                (KeyCode::Left, _) => Ok(Self::Move(Direction::Left)),
+                (KeyCode::Right, _) => Ok(Self::Move(Direction::Right)),
+                (KeyCode::PageUp, _) => Ok(Self::Move(Direction::PageUp)),
+                (KeyCode::PageDown, _) => Ok(Self::Move(Direction::PageDown)),
+                (KeyCode::Home, _) => Ok(Self::Move(Direction::Home)),
+                (KeyCode::End, _) => Ok(Self::Move(Direction::End)),
+                _ => Err(format!("Key code not supported: {code:?}")),
+            },
+            Event::Resize(width_u16, height_u16) => {
+                #[allow(clippy::as_conversions)]
+                let width = width_u16 as usize;
+                #[allow(clippy::as_conversions)]
+                let height = height_u16 as usize;
+                Ok(Self::Resize(Size { width, height }))
+            }
+            _ => Err(format!("Unsupported event: {event:?}")),
+        }
     }
 }
