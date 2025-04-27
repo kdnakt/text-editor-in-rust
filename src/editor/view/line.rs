64610a03c696b3e475987f7fb2e9a05aa0b2a982
kdnakt <a.kid.1985@gmail.com>
2025-04-27 13:21:05 +0900

    handle grapheme

--- src/editor/view/line.rs
+++ src/editor/view/line.rs
@@ -1,23 +1,58 @@
-use std::{cmp, ops::Range};
+
+use unicode_segmentation::UnicodeSegmentation;
+use unicode_width::UnicodeWidthStr;
+
+#[derive(Clone, Copy)]
+enum GraphemeWidth {
+    Half,
+    Full,
+}
+
+impl GraphemeWidth {
+    fn saturating_add(self, other: usize) -> usize {
+        match self {
+            GraphemeWidth::Half => other.saturating_add(1),
+            GraphemeWidth::Full => other.saturating_add(2),
+        }
+    }
+}
+
+struct TextFragment {
+    grapheme: String,
+    rendered_width: GraphemeWidth,
+    replacement: Option<char>,
+}
 
 pub struct Line {
-    string: String,
+    fragments: Vec<TextFragment>,
 }
 
 impl Line {
     pub fn from(line_str: &str) -> Self {
-        Self {
-            string: String::from(line_str),
-        }
+        let fragments = line_str
+            .graphemes(true)
+            .map(|grapheme| {
+                let unicode_width = grapheme.width();
+                let rendered_width = match unicode_width {
+                    0 | 1 => GraphemeWidth::Half,
+                    _ => GraphemeWidth::Full,
+                };
+                let replacement = match unicode_width {
+                    0 => Some('·'),
+                    _ => None,
+                };
+                TextFragment {
+                    grapheme: grapheme.to_string(),
+                    rendered_width,
+                    replacement,
+                }
+            })
+            .collect();
+        Self { fragments }
     }
 
-    pub fn get(&self, range: Range<usize>) -> String {
-        let start = range.start;
-        let end = cmp::min(range.end, self.string.len());
-        self.string.get(start..end).unwrap_or_default().to_string()
+    pub fn grapheme_count(&self) -> usize {
+        self.fragments.len()
     }
 
-    pub fn len(&self) -> usize {
-        self.string.len()
-    }
 }
