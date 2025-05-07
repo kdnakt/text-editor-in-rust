ff3293a114badf52b7470dadd6be970ad92505b2
kdnakt <a.kid.1985@gmail.com>
2025-05-08 00:24:43 +0900

    add file name field to buffer

--- src/editor/view/buffer.rs
+++ src/editor/view/buffer.rs
@@ -1,10 +1,14 @@
-use std::{fs::read_to_string, io::Error};
+use std::{
+    fs::{read_to_string, File},
+    io::{Error, Write},
+};
 
 use super::{line::Line, Location};
 
 #[derive(Default)]
 pub struct Buffer {
     pub lines: Vec<Line>,
+    file_name: Option<String>,
 }
 
 impl Buffer {
@@ -14,7 +18,20 @@ impl Buffer {
         for value in contents.lines() {
             lines.push(Line::from(value));
         }
-        Ok(Self { lines })
+        Ok(Self {
+            lines,
+            file_name: Some(file_name.to_string()),
+        })
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        if let Some(file_name) = &self.file_name {
+            let mut file = File::create(file_name)?;
+            for line in &self.lines {
+                writeln!(file, "{}", line)?;
+            }
+        }
+        Ok(())
     }
 
     pub fn insert_char(&mut self, character: char, at: Location) {
