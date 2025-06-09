1402d2bd865c77173ccb8d3d8219b5ff419e84af
kdnakt <a.kid.1985@gmail.com>
2025-06-10 00:06:41 +0900

    impl TryFrom for Move

--- src/editor/command.rs
+++ src/editor/command.rs
@@ -14,6 +14,33 @@ pub enum Move {
     Right,
 }
 
+impl TryFrom<KeyEvent> for Move {
+    type Error = String;
+
+    fn try_from(event: KeyEvent) -> Result<Self, Self::Error> {
+        let KeyEvent {
+            code, modifiers, ..
+        } = event;
+        if modifiers == KeyModifiers::NONE {
+            match code {
+                KeyCode::Up => Ok(Self::Up),
+                KeyCode::Down => Ok(Self::Down),
+                KeyCode::Left => Ok(Self::Left),
+                KeyCode::Right => Ok(Self::Right),
+                KeyCode::PageUp => Ok(Self::PageUp),
+                KeyCode::PageDown => Ok(Self::PageDown),
+                KeyCode::Home => Ok(Self::Home),
+                KeyCode::End => Ok(Self::End),
+                _ => Err(format!("Unsupported key code for Move: {:?}", event.code)),
+            }
+        } else {
+            Err(format!(
+                "Unsupported key code {code:?} or modifier {modifiers:?}"
+            ))
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 pub enum Command {
     Move(Move),
