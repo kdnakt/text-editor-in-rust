2698ff25a9be439c70e02ceffcf4ffbdae408174
kdnakt <a.kid.1985@gmail.com>
2025-06-10 09:30:01 +0900

    add System and Edit command

--- src/editor/command.rs
+++ src/editor/command.rs
@@ -42,6 +42,64 @@ impl TryFrom<KeyEvent> for Move {
 }
 
 #[derive(Clone, Copy)]
+pub enum Edit {
+    Insert(char),
+    InsertNewLine,
+    Backspace,
+    Delete,
+}
+
+impl TryFrom<KeyEvent> for Edit {
+    type Error = String;
+
+    fn try_from(event: KeyEvent) -> Result<Self, Self::Error> {
+        let KeyEvent {
+            code, modifiers, ..
+        } = event;
+        match (code, modifiers) {
+            (KeyCode::Char(character), KeyModifiers::NONE | KeyModifiers::SHIFT) => {
+                Ok(Self::Insert(character))
+            }
+            (KeyCode::Tab, KeyModifiers::NONE) => Ok(Self::Insert('\t')),
+            (KeyCode::Enter, KeyModifiers::NONE) => Ok(Self::InsertNewLine),
+            (KeyCode::Backspace, KeyModifiers::NONE) => Ok(Self::Backspace),
+            (KeyCode::Delete, KeyModifiers::NONE) => Ok(Self::Delete),
+            _ => Err(format!(
+                "Unsupported key code {code:?} with modifiers {modifiers:?}"
+            )),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum System {
+    Save,
+    Resize(Size),
+    Quit,
+}
+
+impl TryFrom<KeyEvent> for System {
+    type Error = String;
+
+    fn try_from(event: KeyEvent) -> Result<Self, Self::Error> {
+        let KeyEvent {
+            code, modifiers, ..
+        } = event;
+        if modifiers == KeyModifiers::CONTROL {
+            match code {
+                KeyCode::Char('s') => Ok(Self::Save),
+                KeyCode::Char('q') => Ok(Self::Quit),
+                _ => Err(format!("Unsupported CONTROL+{code:?} combination")),
+            }
+        } else {
+            Err(format!(
+                "Unsupported key code {code:?} or modifier {modifiers:?}"
+            ))
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
 pub enum Command {
     Move(Move),
     Resize(Size),
