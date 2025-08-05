aee451fa5d47ca2f6b34aa120829ff9470f911ad
kdnakt <a.kid.1985@gmail.com>
2025-08-05 10:00:39 +0900

    move command to mod.rs

--- src/editor/command/mod.rs
+++ src/editor/command/mod.rs
@@ -1,5 +1,8 @@
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
 
+mod system;
+pub use system::System;
+
 use super::size::Size;
 
 #[derive(Clone, Copy)]
@@ -67,39 +70,6 @@ impl TryFrom<KeyEvent> for Edit {
             _ => Err(format!(
                 "Unsupported key code {code:?} with modifiers {modifiers:?}"
             )),
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-pub enum System {
-    Save,
-    Search,
-    Resize(Size),
-    Quit,
-    Dismiss,
-}
-
-impl TryFrom<KeyEvent> for System {
-    type Error = String;
-
-    fn try_from(event: KeyEvent) -> Result<Self, Self::Error> {
-        let KeyEvent {
-            code, modifiers, ..
-        } = event;
-        if modifiers == KeyModifiers::CONTROL {
-            match code {
-                KeyCode::Char('s') => Ok(Self::Save),
-                KeyCode::Char('q') => Ok(Self::Quit),
-                KeyCode::Char('f') => Ok(Self::Search),
-                _ => Err(format!("Unsupported CONTROL+{code:?} combination")),
-            }
-        } else if modifiers == KeyModifiers::NONE && matches!(code, KeyCode::Esc) {
-            Ok(Self::Dismiss)
-        } else {
-            Err(format!(
-                "Unsupported key code {code:?} or modifier {modifiers:?}"
-            ))
         }
     }
 }
---
+++ src/editor/command/system.rs
@@ -0,0 +1,36 @@
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+
+use crate::editor::size::Size;
+
+#[derive(Clone, Copy)]
+pub enum System {
+    Save,
+    Search,
+    Resize(Size),
+    Quit,
+    Dismiss,
+}
+
+impl TryFrom<KeyEvent> for System {
+    type Error = String;
+
+    fn try_from(event: KeyEvent) -> Result<Self, Self::Error> {
+        let KeyEvent {
+            code, modifiers, ..
+        } = event;
+        if modifiers == KeyModifiers::CONTROL {
+            match code {
+                KeyCode::Char('s') => Ok(Self::Save),
+                KeyCode::Char('q') => Ok(Self::Quit),
+                KeyCode::Char('f') => Ok(Self::Search),
+                _ => Err(format!("Unsupported CONTROL+{code:?} combination")),
+            }
+        } else if modifiers == KeyModifiers::NONE && matches!(code, KeyCode::Esc) {
+            Ok(Self::Dismiss)
+        } else {
+            Err(format!(
+                "Unsupported key code {code:?} or modifier {modifiers:?}"
+            ))
+        }
+    }
+}
