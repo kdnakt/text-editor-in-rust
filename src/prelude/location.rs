51cd2005a0cc84949e14cd43e85b03e70b634294
kdnakt <a.kid.1985@gmail.com>
2025-08-17 16:17:31 +0900

    use LineIdx

--- src/prelude/location.rs
+++ src/prelude/location.rs
@@ -1,5 +1,7 @@
+use super::{GraphemeIdx, LineIdx};
+
 #[derive(Clone, Copy, Default)]
 pub struct Location {
-    pub grapheme_index: usize,
-    pub line_index: usize,
+    pub grapheme_index: GraphemeIdx,
+    pub line_index: LineIdx,
 }
