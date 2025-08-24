4eb4df352d86325e3bae149f368c31da91761bb1
kdnakt <a.kid.1985@gmail.com>
2025-08-24 11:50:05 +0900

    add Highlighter

---
+++ src/editor/uicomponents/view/highlighter.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+use crate::editor::Annotation;
+use crate::prelude::*;
+
+pub struct Highlighter<'a> {
+    matched_word: Option<&'a str>,
+    selected_match: Option<Location>,
+    highlights: HashMap<LineIdx, Vec<Annotation>>,
+}
+
+impl<'a> Highlighter<'a> {
+    pub fn new(matched_word: Option<&'a str>, selected_match: Option<Location>) -> Self {
+        Highlighter {
+            matched_word,
+            selected_match,
+            highlights: HashMap::new(),
+        }
+    }
+
+    pub fn get_annotations(&self, line_index: LineIdx) -> Option<&Vec<Annotation>> {
+        self.highlights.get(&line_index)
+    }
+}
